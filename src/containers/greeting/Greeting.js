4fcaee3d0dd6fa04916e9c73876174a536993074
Priyadharshini0101 <priyadharshinis5102@gmail.com>
2025-04-19 14:28:32 +0530

    Updated Files

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -42,8 +42,9 @@ export default function Greeting() {
                 <Button text="Contact me" href="#contact" />
                 {greeting.resumeLink && (
                   <a
-                    href={require("../../assets/resume.pdf")}
+                    href={require("../../../public/resume.pdf")}
                     download="Resume.pdf"
+                    type="application/pdf" 
                     className="download-link-button"
                   >
                     <Button text="Download my resume" />
